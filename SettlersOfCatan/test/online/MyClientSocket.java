f47b67cc5048acf44a2ad8c779e77910ca8ffb6a
alpingencer <alpingencer@jupiter>
2019-06-14 13:32:54 +0200

    Socket Test

---
+++ SettlersOfCatan/test/online/MyClientSocket.java
@@ -0,0 +1,37 @@
+package online;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class MyClientSocket {
+	private Socket socket;
+	private Scanner scanner;
+
+	private MyClientSocket(InetAddress serverAddress, int serverPort) throws Exception {
+		this.socket = new Socket(serverAddress, serverPort);
+		this.scanner = new Scanner(System.in);
+	}
+
+	private void start() throws IOException {
+		String input;
+		while (true) {
+			input = scanner.nextLine();
+			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+			out.println(input);
+			out.flush();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		String serverIP = "192.168.7.169";
+		String port = "41875";
+		MyClientSocket client = new MyClientSocket(InetAddress.getByName(serverIP), Integer.parseInt(port));
+
+		System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+		client.start();
+	}
+}
